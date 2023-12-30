a17b215426332d179f28c002316b2debd7abdeea
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-12-30 23:24:16 +0100

    build(compat-threading): Create the threading module

---
+++ compat-threading/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	alias(opensavvyConventions.plugins.base)
+	alias(opensavvyConventions.plugins.kotlin.library)
+}
+
+kotlin {
+	jvm()
+
+	sourceSets.commonMain {
+		dependencies {
+			api(projects.core)
+		}
+	}
+
+	sourceSets.commonTest {
+		dependencies {
+			implementation(libs.prepared.kotest)
+		}
+	}
+}
+
+library {
+	name.set("Sentier for threads")
+	description.set("Kotlin Multiplatform observability API embedded in thread locals")
+	homeUrl.set("https://gitlab.com/opensavvy/sentier")
+
+	license.set {
+		name.set("Apache 2.0")
+		url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+	}
+}
